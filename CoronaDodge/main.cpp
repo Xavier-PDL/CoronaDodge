6c905cec7fcf81907aabc9b34a1a5f0405ab2797
Xavier P <XavierPDL@gmail.com>
2020-09-25 21:09:08 -0400

    Player input implemented

--- CoronaDodge/main.cpp
+++ CoronaDodge/main.cpp
@@ -13,15 +13,16 @@
 #pragma comment(lib, "sfml-graphics-s.lib")
 #endif
 
-int main()
+sf::Sprite player;
+sf::Sprite enemy1;
+sf::Sprite enemy2;
+sf::Texture playerTexture;
+sf::Texture enemyTexture;
+
+constexpr auto moveSpeed = 150.0f;
+
+void createEntities()
 {
-	sf::Sprite player;
-	sf::Sprite enemy1;
-	sf::Sprite enemy2;
-	sf::Texture playerTexture;
-	sf::Texture enemyTexture;
-	sf::RenderWindow window(sf::VideoMode(800, 600), "CoronaDodge!");
-	
 	playerTexture.loadFromFile("assets/player.bmp");
 	enemyTexture.loadFromFile("assets/enemy.bmp");
 
@@ -33,20 +34,71 @@ int main()
 	enemy2.setColor(sf::Color::Red);
 	enemy2.setTexture(enemyTexture);
 	enemy1.setPosition({ 65.0f, 155.0f });
+}
 
+void drawEntities(sf::RenderWindow& window)
+{
+	window.draw(player);
+	window.draw(enemy1);
+	window.draw(enemy2);
+}
+
+void handleInput(sf::Time dt)
+{
+	sf::Vector2f playerVel;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+	{
+		playerVel.y -= moveSpeed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+	{
+		playerVel.y += moveSpeed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+	{
+		playerVel.x -= moveSpeed;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+	{
+		playerVel.x += moveSpeed;
+	}
+
+	if (abs(playerVel.x) + abs(playerVel.y) > moveSpeed)
+	{
+		playerVel *= 0.75f;
+	}
+
+	player.move(playerVel * dt.asSeconds());
+}
+
+void updateEntities(sf::Time dt)
+{
+	// Todo
+}
+
+int main()
+{
+	sf::Clock clock;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "CoronaDodge!");
+	createEntities();
 	while (window.isOpen())
 	{
+		sf::Time dt = clock.restart();
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		handleInput(dt);
 		window.clear(sf::Color::White);
 
-		window.draw(player);
-		window.draw(enemy1);
-		window.draw(enemy2);
+		drawEntities(window);
 
 		window.display();
 	}
