9e7058baca47d98059ede93f068617a47588aa2f
Xavier P <XavierPDL@gmail.com>
2020-09-25 20:33:15 -0400

    Enemies drawn on screen

--- CoronaDodge/main.cpp
+++ CoronaDodge/main.cpp
@@ -16,12 +16,24 @@
 int main()
 {
 	sf::Sprite player;
+	sf::Sprite enemy1;
+	sf::Sprite enemy2;
 	sf::Texture playerTexture;
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World!");
+	sf::Texture enemyTexture;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "CoronaDodge!");
 	
 	playerTexture.loadFromFile("assets/player.bmp");
+	enemyTexture.loadFromFile("assets/enemy.bmp");
+
 	player.setColor(sf::Color::Blue);
 	player.setTexture(playerTexture);
+	enemy1.setColor(sf::Color::Red);
+	enemy1.setTexture(enemyTexture);
+	enemy1.setPosition({ 25.0f, 55.0f });
+	enemy2.setColor(sf::Color::Red);
+	enemy2.setTexture(enemyTexture);
+	enemy1.setPosition({ 65.0f, 155.0f });
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -31,7 +43,11 @@ int main()
 				window.close();
 		}
 		window.clear(sf::Color::White);
+
 		window.draw(player);
+		window.draw(enemy1);
+		window.draw(enemy2);
+
 		window.display();
 	}
 
